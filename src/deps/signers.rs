cf7ba531bfe694c63f3c8b0d697fc5f98a3b818e
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-04-25 12:30:15 +0300

    updated-libsigners-injection

--- src/deps/signers.rs
+++ src/deps/signers.rs
@@ -1,17 +1,17 @@
 use std::sync::Arc;
 
-use libsigners::{HS256Signer, RS256Validator,  RS256Signer};
-pub use libsigners::{Validate,Sign};
 use crate::di::{AsyncFromEnv, EnvError};
+use libsigners::{HS256Signer, RS256Signer, RS256Validator};
+pub use libsigners::{Sign, Validate};
 
 impl AsyncFromEnv for Arc<dyn Validate> {
     async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self, crate::di::EnvError> {
         let signer_type = ctx.get("validate.type")?;
         match signer_type {
-            "hs256" => Ok(
-                Arc::new(HS256Signer::new(ctx.get("validate.aud")?.to_owned()))
-                    as Arc<dyn Validate>,
-            ),
+            "hs256" => Ok(Arc::new(HS256Signer::new(
+                ctx.get("validate.aud")?.to_owned(),
+                ctx.get("validate.secret")?.to_owned(),
+            )) as Arc<dyn Validate>),
             "rs256" => Ok(Arc::new(RS256Validator::new(
                 ctx.get("validate.public_key")?.to_string(),
                 ctx.get("validate.aud")?.to_string(),
@@ -27,9 +27,10 @@ impl AsyncFromEnv for Arc<dyn Sign> {
     async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self, crate::di::EnvError> {
         let signer_type = ctx.get("sign.type")?;
         match signer_type {
-            "hs256" => {
-                Ok(Arc::new(HS256Signer::new(ctx.get("sign.aud")?.to_owned())) as Arc<dyn Sign>)
-            }
+            "hs256" => Ok(Arc::new(HS256Signer::new(
+                ctx.get("sign.aud")?.to_owned(),
+                ctx.get("validate.secret")?.to_owned(),
+            )) as Arc<dyn Sign>),
             "rs256" => Ok(Arc::new(RS256Signer::new(
                 ctx.get("sign.private_key")?.to_string(),
                 ctx.get("sign.aud")?.to_string(),
@@ -40,4 +41,3 @@ impl AsyncFromEnv for Arc<dyn Sign> {
         }
     }
 }
-
